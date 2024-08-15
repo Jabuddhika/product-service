da8d5cfe0b254f22636cc25da6be009d97a4988e
Buddhika Sampath jayasinghe <123095612+Jabuddhika@users.noreply.github.com>
2024-08-15 19:27:21 +0530

    initialize the project

---
+++ src/test/java/com/efutures/products/service/EfuturesProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efutures.products.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfuturesProductsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
